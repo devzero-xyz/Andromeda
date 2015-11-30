d329f8a9d98f1e649f563f91d29857ff34e2e0dd
BWBellairs <BWBellairs@outlook.com>
2015-11-30 19:30:12 +0000

    update

--- main.py
+++ main.py
@@ -156,14 +156,14 @@ while True:
                     irc.send("PRIVMSG {0} :{1}, INVALID: syntax. USAGE: *kick <nickname> [reason]\r\n".format(chan, nickname).encode("UTF-8"))
 
             elif command[0] == "op":
-                if not command[1]:
+                if command[1] == "":
                     irc.send("MODE {0} +o {1} :\r\n".format(chan, nickname).encode("UTF-8"))
 
                 elif command[1]:
                     irc.send("MODE {0} +o {1} :\r\n".format(chan, command[1]).encode("UTF-8"))
 
             elif command[0] == "deop":
-                if not command[1]:
+                if command[1] == "":
                     irc.send("MODE {0} -o {1} :\r\n".format(chan, nickname).encode("UTF-8"))
 
                 elif command[1]:
