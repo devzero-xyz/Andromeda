962518ffb1d8a55e5d6452736dae2f441a325a65
BWBellairs <BWBellairs@outlook.com>
2015-12-11 20:21:37 +0000

    Added code
    Modules is not being imported?

--- main.py
+++ main.py
@@ -8,28 +8,27 @@ importantModules = {
 plugins = {
     }
 importQueue = [plugins, importantModules]
-def importModules():
-    for module in importQueue:
-            for i in module:
-                if len(i) >= 2:
-                    moduleInfo = i.split()
-                    moduleLocation = moduleInfo[0]
-                    moduleForImport = moduleInfo[1]
+def importModules(link):
+    for i in link:
+        if len(i) >= 2:
+            moduleInfo = i.split()
+            moduleLocation = moduleInfo[0]
+            moduleForImport = moduleInfo[1]
+            try:
+                exec("from " + moduleLocation + " import " + moduleForImport)
+                print("Module " + moduleForImport + " from "  + moduleLocation + " has been imported")
+            except:
+                print("Module " + moduleForImport + " from " + moduleLocation + " could not be imported")
+        else:
+            try:
+                exec("import " + i)
+                print("Module " + moduleForImport + " has been imported")
 
-                    try:
-                        exec("from " + moduleLocation + " import " + moduleForImport)
-                        print("Module " + moduleForImport + " from "  + moduleLocation + " has been imported")
+            except:
+                print("Module " + moduleForImport + " could not be imported")
 
-                    except:
-                        print("Module " + moduleForImport + " from " + moduleLocation + " could not be imported")
-
-                else:
-                    try:
-                        exec("import " + i)
-                        print("Module " + moduleForImport + " has been imported")
-
-                    except:
-                        print("Module " + moduleForImport + " could not be imported")
+importModules(importantModules)
+importModules(plugins)
 
 ircRelay.ircSend("QUIT") #In case of the bot being reloaded
 ircRelay.connectAndIdentify()
